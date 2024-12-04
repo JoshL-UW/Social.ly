8de31e0d334c64ff46428a1713273b0a0b59c653
maxlevy <maxllevy02@gmail.com>
2024-12-04 02:33:25 -0600

    Began implementing NFC. Permissions updated in AndroidManifest.xml and some changes made to MainActivity.kt.

--- app/src/main/java/com/cs407/socially/MainActivity.kt
+++ app/src/main/java/com/cs407/socially/MainActivity.kt
@@ -5,12 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.nfc.NfcAdapter
+import android.widget.Toast
 
+private var nfcAdapter: NfcAdapter? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get NFC adapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        // Check if NFC is available on the device
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC not available on this device.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // Check if NFC is enabled
+        if (!nfcAdapter!!.isEnabled) {
+            Toast.makeText(this, "NFC is disabled. Please enable it in settings.", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
